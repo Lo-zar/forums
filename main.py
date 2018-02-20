0bfdf25b06562cdd9442713e73efbd99bca15186
La-zar <sizer2189@gmail.com>
2018-02-20 15:06:41 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,8 @@
+from models import memper, post
+
+member1 = member("SIZER", "25")
+member2 = member("LO-ZAR", "24")
+
+post1 = post("Test", "This text is a test of the quality of work")
+post2 = post("help my", "whan you walk on a stret and...... next time ")
+post3 = post("Freedom", "From the first line of the first chapter of the Green Book")
