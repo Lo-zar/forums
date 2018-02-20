3c2fea4549a084d7d7673ef8f47dcdaab2b2542b
La-zar <sizer2189@gmail.com>
2018-02-20 16:34:54 +0200

    Update models.py

--- models.py
+++ models.py
@@ -1,10 +1,13 @@
 class Member:
     def __init__(self, name, age):
+        self.member_id = id
         self.name = name
         self.age = age
+        self.member_posts = []
 
 
 class Post:
     def __init__(self, title, body):
+        self.post_id = id
         self.title = title
         self.body = body
