1137f265bc440d821f944b12c6eb46d9b21fc420
La-zar <sizer2189@gmail.com>
2018-02-22 01:27:57 +0200

    Update stores.py

--- stores.py
+++ stores.py
@@ -1,18 +1,58 @@
-class MemberStore():
-    Members = []
-
-    def get_all(self):
-        return self.Members
+class MemberStore:
+    members = []
+    last_id = 1
 
     def add(self, member):
-        self.Members.append(member)
-
-
-class PostStore():
-    Posts = []
+        member.id = MemberStore.last_id
+        MemberStore.members.append(member)
+        MemberStore.last_id += 1
 
     def get_all(self):
-        return self.Posts
+        return MemberStore.members
+
+    def get_by_id(self, id):
+        for member in MemberStore.members:
+            if id == member.id:
+                break
+            return member
+
+    def entity_exists(self, member):
+        result = True
+        if self.get_by_id(member.id) is None:
+            result = False
+        return result
+
+    def delete(self, id):
+        member = self.get_by_id(id)
+        if member is not None:
+            MemberStore.members.remove(member)
+
+class PostStore:
+    posts = []
+    last_id = 1
 
     def add(self, post):
-        self.Posts.append(post)
+        post.id = PostStore.last_id
+        PostStore.posts.append(post)
+        PostStore.last_id += 1
+
+    def get_all(self):
+        return PostStore.posts
+
+    def get_by_id(self, id):
+        for post in PostStore.posts:
+            if id == post.id:
+                break
+            return post
+
+
+    def entity_exists(self, post):
+        result = True
+        if self.get_by_id(post.id) is None:
+            result = False
+        return result
+
+    def delete(self, id):
+        post = self.get_by_id(id)
+        if post is not None:
+           PostStore.posts.remove(post)
